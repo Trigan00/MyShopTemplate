403abd95ee8cab0842d114c8d0d5b22c42bd128a
Ayaz Mubarak <ayaz.mubarakov@gmail.com>
2022-08-08 19:13:58 +0300

    add products.json

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -2,12 +2,14 @@ import styles from "./Products.module.css";
 
 function Product(props) {
   const onSectionCLickHandler = () => {
-    console.log("message");
+    console.log(props.imgurl);
   };
 
   return (
     <div className={styles.Item} onClick={onSectionCLickHandler}>
-      <div className={styles.image} />
+      <div className={styles.image}>
+        <img src={props.imgurl} alt="MyPicture"></img>
+      </div>
       <div className={styles.Name}>{props.name}</div>
       <div className={styles.Price}>{props.price} &#8381;</div>
     </div>
