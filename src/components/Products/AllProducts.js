3611b2c6ae117ba396d03df31130b46f40773611
Ayaz Mubarak <ayaz.mubarakov@gmail.com>
2022-08-08 22:00:38 +0300

    add fetch

--- src/components/Products/AllProducts.js
+++ src/components/Products/AllProducts.js
@@ -1,23 +1,38 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
 import Product from "./Product";
 
-import allProductsJSON from "../../data/products.json";
+// import allProductsJSON from "../../data/products.json";
 
 function AllProducts(props) {
-  const newArr = allProductsJSON.filter(
-    (product) => product.SectionName === props.selectedSection
-  );
+  const [data, SetData] = useState(null);
 
+  useEffect(() => {
+    fetch("https://trigan00.github.io/jsonapi/products.json")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        SetData(data);
+      });
+    // .catch((err) => console.log(err));
+  }, []);
+
+  const newArr =
+    data &&
+    data.filter((product) => product.SectionName === props.selectedSection);
+  //useEffect надо перенести в app, чтобы данные загрузились сразу после открытия страницы
+  //map надо перенести в компонент Product
   return (
     <Fragment>
-      {newArr.map((product) => (
-        <Product
-          name={product.name}
-          price={product.price}
-          key={product.id}
-          imgurl={product.imgURL}
-        />
-      ))}
+      {newArr &&
+        newArr.map((product) => (
+          <Product
+            name={product.name}
+            price={product.price}
+            key={product.id}
+            imgurl={product.imgURL}
+          />
+        ))}
     </Fragment>
   );
 }
